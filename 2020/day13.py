2695244d2970d529e59b0a1e997a8f974f496534
Matt Mackenzie <mbm2228@columbia.edu>
2021-12-01 00:25:38 -0500

    day 1, 2021

---
+++ 2020/day13.py
@@ -0,0 +1,79 @@
+"""Day 13"""
+import pytest
+from aoc.solution import Solution
+
+TEST_DATA = """
+939
+67,x,7,59,61
+""".strip()
+
+
+def get_first_timestamp(busses: list[int]) -> int:
+    """Get the first timestamp"""
+    return NotImplemented
+
+
+def get_best_possible_time(bus_number: int, min_time: int) -> int:
+    """Get the best possible time"""
+    if min_time % bus_number == 0:
+        return min_time
+    else:
+        return (min_time // bus_number + 1) * bus_number
+
+
+class Day13(Solution):
+    """Solution to day 13 of the 2020 Advent of Code"""
+
+    min_time: int
+
+    def __init__(self) -> None:
+        super().__init__(2020, 13, "")
+
+    def _part_one(self) -> int:
+        """TODO"""
+        busses = [x for x in self.data if x > 0]
+        best_times = [
+            get_best_possible_time(bus_number, self.min_time) for bus_number in busses
+        ]
+
+        best_time = min(best_times)
+        best_bus = busses[best_times.index(best_time)]
+
+        return (best_time - self.min_time) * best_bus
+
+    def _part_two(self) -> int:
+        """Return earliest time stamp"""
+        return NotImplemented
+
+    def _get_data(self) -> list[int]:
+        data = self.input.as_list()
+        self.min_time = int(data[0])
+
+        return [int(x) if x != "x" else -1 for x in data[1].split(",")]
+
+
+def test_solution() -> None:
+    """Test the solution"""
+    solution = Day13()
+    solution.set_input_data(TEST_DATA.split("\n"))
+
+    assert solution.part_one() == 295
+    assert solution.part_two() == 1068781
+
+
+@pytest.mark.parametrize(
+    "busses, expected",
+    [
+        ([17, -1, 13, 19], 3417),
+        ([67, 7, 59, 61], 754018),
+        ([67, -1, 7, 59, 61], 779210),
+        ([67, 7, -1, 59, 61], 1261476),
+        ([1789, 37, 47, 1889], 1202161486),
+    ],
+)
+def test_get_first_timestamp(busses: list[int], expected: int) -> None:
+    assert get_first_timestamp(busses) == expected
+
+
+if __name__ == "__main__":
+    test_solution()
---
+++ 2021/day01.py
@@ -0,0 +1,59 @@
+"""Day 1: Sonar Sweep"""
+from aoc.solution import Solution
+
+
+TEST_DATA = """
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+""".strip()
+
+
+def get_num_increases(data: list[int]) -> int:
+    """
+    Get the total number of times the value of the next element
+    in the list is greater than the value of the previous element
+
+    >>> get_num_increases([1, 3, 4, 2, 5])
+    3
+    """
+    return sum(b > a for a, b in zip(data[:-1], data[1:]))
+
+
+class Day01(Solution):
+    """Solution to day 1 of the 2021 Advent of Code"""
+
+    def __init__(self) -> None:
+        super().__init__(2021, 1, "Sonar Sweep")
+
+    def _part_one(self) -> int:
+        """How many measurements are larger than the previous measurement?"""
+        return get_num_increases(self.data)
+
+    def _part_two(self) -> int:
+        """How many increases if you use the sum of windows of three consecutive measurements?"""
+        windows = zip(self.data[:-2], self.data[1:-1], self.data[2:])
+        return get_num_increases([sum(w) for w in windows])
+
+    def _get_data(self) -> list[int]:
+        return self.input.as_list(int)
+
+
+def test_solution(data: str) -> None:
+    """Test the solution"""
+    solution = Day01()
+    solution.set_input_data(data.split("\n"))
+
+    assert solution.part_one() == 7
+    assert solution.part_two() == 5
+
+
+if __name__ == "__main__":
+    test_solution(TEST_DATA)
